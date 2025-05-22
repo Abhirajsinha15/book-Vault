314006061d533cc08a0bb164ae49ad9c7889b31e
Abhiraj <abhirajsinha15@gmail.com>
2025-05-22 17:38:32 +0530

    basic ui of cart page

--- js/cart.js
+++ js/cart.js
@@ -7,18 +7,44 @@ document.addEventListener("DOMContentLoaded" , function (){
 
 
         const likedBooks = JSON.parse(localStorage.getItem("likedBooks")) || [];
-    const cartContainer = document.querySelector(".cart-container");
+    const cartContainer = document.getElementById("cartContainer");
 
     likedBooks.forEach(book => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-        <img src="${book.image}" />
-        <h2>${book.title}</h2>
-    `;
-    cartContainer.appendChild(card);
-    console.log(card)
+     const col = document.createElement('div');
+          col.className = 'col-md-6 col-lg-4 col-xl-4';
+
+          col.innerHTML = `
+            <div class="card mb-4">
+              <div class="card-img">
+                <img src="${book.image}" alt="${book.title}" class="img-fluid">
+              </div>
+              <div class="card-desc p-2">
+              <div class = "d-flex justify-content-between">
+                  <h2 style="font-size:1.2rem;">${book.title}</h2>  <i class="fa fa-heart" id="goToCartBtn" style="cursor: pointer;" title="Add to Cart" aria-hidden="true"></i>
+              </div>
+              
+                <h3 style="font-size:1rem; color:gray;">${book.author}</h3>
+              </div>
+            </div>
+          `;
+
+          cartContainer.appendChild(col);
     });
+
+    
+
+cartContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("fa-heart-o")) {
+    e.target.classList.remove("fa-heart-o");
+    e.target.classList.add("fa-heart");
+
+  } else if (e.target.classList.contains("fa-heart")) {
+    e.target.classList.remove("fa-heart");
+    e.target.classList.add("fa-heart-o");
+  }
+
+});
+
     
 
   function logout(){
